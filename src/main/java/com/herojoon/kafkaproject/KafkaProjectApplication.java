82f6ad136de7a8dbd432ad1dacdfb300dd203af0
herojoon <herojoon@gmail.com>
2022-04-05 23:59:11 +0900

    create project

---
+++ src/main/java/com/herojoon/kafkaproject/KafkaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.herojoon.kafkaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaProjectApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/herojoon/kafkaproject/producer/MessageProducer.java
@@ -0,0 +1,32 @@
+package com.herojoon.kafkaproject.producer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Kafka Producer 테스트를 위한 Controller
+ */
+@RequestMapping("kafka")
+@RestController
+@RequiredArgsConstructor
+public class MessageProducer {
+    // Spring application.yaml에 정의한 kafka 설정이 주입된 kafkaTemplate
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    private static String TOPIC_NAME = "dev-topic";
+
+    /**
+     * Kafka Producer
+     * Kafka로 메시지를 전달하는 역할
+     * @return
+     */
+    @GetMapping("producer")
+    public String sendMessage() {
+        String messageData = "kafka message";
+        kafkaTemplate.send(TOPIC_NAME, messageData);
+        return "success.";
+    }
+}
